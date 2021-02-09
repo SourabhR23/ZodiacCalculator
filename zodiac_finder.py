3b72b9f2b2d02765b7158f24e1642338f228527f
SourabhR23 <64792134+SourabhR23@users.noreply.github.com>
2021-02-10 01:24:16 +0530

    Add files via upload

---
+++ zodiac_finder.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+root = Tk()
+root.title("ZODIAC SIGN CALCULATOR")
+root.geometry("400x400")
+root.configure(bg="white")
+
+result = Label(root, bg="white")
+result.grid(row=4, column=0, columnspan=2)
+
+if __name__ == '__main__':
+    root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
